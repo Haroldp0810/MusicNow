d69356a9df17a5f97ec071f1222d970e9fad2a8b
Haroldp0810 <Harold.p2329@gmail.com>
2022-03-05 17:12:23 -0500

    Iniciando

---
+++ src/main/java/com/musiscnow/MusicNow/MusicNowApplication.java
@@ -0,0 +1,13 @@
+package com.musiscnow.MusicNow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicNowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musiscnow/MusicNow/MusicNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musiscnow.MusicNow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
